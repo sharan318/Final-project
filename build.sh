942300d41e7bc89fcccbb140706e83f1a2520d40
sharan318 <hariharan318@gmail.com>
2025-08-22 21:30:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ echo "building the images..."
 
 if [ "$BRANCH_NAME" = "dev" ]; then
     docker build -t final-dev:latest .
-elif [ "$BRANCH_NAME" = "master" ]; then
+elif [ "$BRANCH_NAME" = "main" ]; then
     docker build -t final-prod:latest .
 fi
